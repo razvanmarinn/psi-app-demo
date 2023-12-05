9b29eafa00e197c70d401c91aadd86d6d1f27625
Razvan <razvanmarin777@gmail.com>
2023-12-05 16:09:27 +0200

    fix

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,14 @@
-from src.main import hello_world
+import sys
+import os
+
+# Get the parent directory of the current file (test.py)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+# Add the parent directory to sys.path
+sys.path.insert(0, parent_dir)
+
+from app.src.main import hello_world
 
 def test_hello_world():
     asd = hello_world()
