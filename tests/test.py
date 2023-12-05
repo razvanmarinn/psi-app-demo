e4a54d72c91e5e87954b049881b50e8b9377afcb
Razvan <razvanmarin777@gmail.com>
2023-12-05 16:41:44 +0200

    added deploy step

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,9 @@
 import sys
 import os
 
-# Get the parent directory of the current file (test.py)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 
-# Add the parent directory to sys.path
 sys.path.insert(0, parent_dir)
 
 from src.main import hello_world
